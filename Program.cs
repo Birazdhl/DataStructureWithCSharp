41edac0518778a949a692102f75b88c1eb7f37bf
biraz <birazdhl@gmail.com>
2026-05-15 05:06:43 +0300

    Dsa first sommit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using DataStructureAndAlgorithm.Recursion;
+
+Recursion rec = new Recursion();
+
+
+///// Tail Recursion ///
+//rec.TailRecursion(4);
+
+///// Head Recursion ///
+//rec.HeadRecursion(4);
+
+////TreeRecursion
+//rec.TreeRecursion(3);
+
+//Factorial
+Console.WriteLine(rec.Factorial(5));
---
+++ Recursion/TreeRecursion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureAndAlgorithm.Recursion
+{
+    public partial class Recursion
+    {
+        //Tail Recursion
+        public void TailRecursion(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine(Math.Pow(n, 2));
+                TailRecursion(n - 1);
+            }
+        }
+
+        //Head Recursion
+        public void HeadRecursion(int n)
+        {
+            if (n > 0)
+            {
+                HeadRecursion(n - 1);
+                Console.WriteLine(Math.Pow(n, 2));
+            }
+        }
+
+        //Tree Recursion
+        public void TreeRecursion(int n)
+        {
+            if (n > 0)
+            {
+                TreeRecursion(n - 1);
+                Console.WriteLine(Math.Pow(n, 2));
+                TreeRecursion(n - 1);
+            }
+        }
+
+        //Factorial
+        public int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            return n + Factorial(n - 1);
+        }
+    }
+}
